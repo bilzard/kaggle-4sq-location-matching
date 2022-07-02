a3d0ff4fb83dd7e13b94ecc24f97045e9b9df986
bilzard <nakamine11@gmail.com>
2022-07-02 21:06:10 +0900

    add model alias

--- ditto_light/ditto.py
+++ ditto_light/ditto.py
@@ -15,6 +15,9 @@ from tqdm.auto import tqdm
 
 # map lm name to huggingface's pre-trained model names
 lm_mp = {
+    "distilbert": "distilbert-base-uncased",
+    "bert": "bert-base-uncased",
+    "albert": "albert-base-v2",
     "roberta": "roberta-base",
     "distilbert": "distilbert-base-uncased",
     "xlmr-base": "xlm-roberta-base",
@@ -22,6 +25,11 @@ lm_mp = {
     "xlmr-xl": "facebook/xlm-roberta-xl",
     "xlmr-xxl": "facebook/xlm-roberta-xxl",
     "multilingual-bert-cased": "bert-base-multilingual-cased",
+    "stsb-xlm-r-multilingual": "sentence-transformers/stsb-xlm-r-multilingual",
+    "all-mpnet-base-v2": "sentence-transformers/all-mpnet-base-v2",
+    "all-distilroberta-v1": "sentence-transformers/all-distilroberta-v1",
+    "all-MiniLM-L6-v2": "sentence-transformers/all-MiniLM-L6-v2",
+    "all-MiniLM-L12-v2": "sentence-transformers/all-MiniLM-L12-v2",
 }
 
 
