f7295071fa460a4bca70bbaf8400f78017e25d89
bilzard <nakamine11@gmail.com>
2022-07-02 18:30:16 +0900

    specify dataset path with cmd args

--- train_ditto.py
+++ train_ditto.py
@@ -12,9 +12,19 @@ sys.path.insert(0, "Snippext_public")
 from ditto_light.dataset import DittoDataset
 from ditto_light.ditto import train
 
+
+def make_task_name(path):
+    """
+    "/data/hoge/hoge_hoge.tsv.gz" -> hoge_hoge
+    """
+    return path.split("/")[-1].split(".")[0]
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--task", type=str, default="Structured/Beer")
+    parser.add_argument("train_path", type=str)
+    parser.add_argument("val_path", type=str)
+    parser.add_argument("test_path", type=str)
     parser.add_argument("--run_id", type=int, default=0)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--max_len", type=int, default=256)
@@ -42,11 +52,11 @@ if __name__ == "__main__":
         torch.cuda.manual_seed_all(seed)
 
     # only a single task for baseline
-    task = hp.task
+    hp.task = make_task_name(hp.train_path)
 
     # create the tag of the run
     run_tag = "%s_lm=%s_da=%s_dk=%s_su=%s_size=%s_id=%d" % (
-        task,
+        hp.task,
         hp.lm,
         hp.da,
         hp.dk,
@@ -56,21 +66,12 @@ if __name__ == "__main__":
     )
     run_tag = run_tag.replace("/", "_")
 
-    # load task configuration
-    configs = json.load(open("configs.json"))
-    configs = {conf["name"]: conf for conf in configs}
-    config = configs[task]
-
-    trainset = config["trainset"]
-    validset = config["validset"]
-    testset = config["testset"]
-
     # load train/dev/test sets
     train_dataset = DittoDataset(
-        trainset, lm=hp.lm, max_len=hp.max_len, size=hp.size, da=hp.da
+        hp.train_path, lm=hp.lm, max_len=hp.max_len, size=hp.size, da=hp.da
     )
-    valid_dataset = DittoDataset(validset, lm=hp.lm)
-    test_dataset = DittoDataset(testset, lm=hp.lm)
+    valid_dataset = DittoDataset(hp.val_path, lm=hp.lm)
+    test_dataset = DittoDataset(hp.test_path, lm=hp.lm)
 
     # train and evaluate the model
     with wandb.init(project="4sq", name=run_tag, config=vars(hp)):
