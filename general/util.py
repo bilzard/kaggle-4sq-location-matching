bc41d372862d5503a3e9b3d47c1a962e30ade05b
bilzard <nakamine11@gmail.com>
2022-07-05 17:10:31 +0900

    bug fix: import_by_name

--- general/util.py
+++ general/util.py
@@ -4,7 +4,7 @@ from humanize import naturalsize
 
 
 def import_by_name(module_path, keyword):
-    target = (importlib.import_module(module_path), keyword)
+    target = getattr(importlib.import_module(module_path), keyword)
     return target
 
 
